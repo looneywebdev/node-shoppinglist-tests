cb8dac869a36348ae7671afec41edd9dd1a2c7bb
Heather Looney <looneydevelopment@gmail.com>
2018-07-08 17:12:50 -0700

    initial recipes commit

---
+++ test/test-recipes.js
@@ -0,0 +1,80 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const { app, runServer, closeServer } = require ('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Recipes', function() {
+    before(function() {
+        return runServer();
+    });
+    after(function () {
+        return closeServer();
+    });
+    it('should list items on GET', function () {
+        return chai
+            .request(app)
+            .get('/recipes')
+            .then(function(res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.an('array');
+
+                expect(res.body.length).to.be.at.least(1);
+                const expectedKeys = ['name', 'id', 'ingredients'];
+                res.body.forEach(function(item) {
+                    expect(item).to.be.an('object');
+                    expect(item).to.include.keys(expectedKeys);
+                });
+                    
+            });
+    });
+    it('should add a recipe on POST', function() {
+        const newRecipe = {
+            name: 'coffee', ingredients: ['ground coffee', 'hot water']};
+        return chai.request(app)
+          .post('/recipes')
+          .send(newRecipe)
+          .then(function(res) {
+            expect(res).to.have.status(201);
+            expect(res).to.be.json;
+            expect(res.body).to.be.an('object');
+            expect(res.body).to.include.keys('name', 'id', 'ingredients');
+            expect(res.body.name).to.be.equal(newRecipe.name);
+            expect(res.body.ingredients).to.be.an('array');
+            expect(res.body.ingredients).to.include.members(newRecipe.ingredients);
+          });
+      });
+      it('should update recipes on PUT', function() {
+        const updateData = {
+          name: 'foo',
+          ingredients: ['bizz', 'bang']
+        };
+        return chai.request(app)
+          .get('/recipes')
+          .then(function(res) {
+            updateData.id = res.body[0].id;
+    
+            return chai.request(app)
+              .put(`/recipes/${updateData.id}`)
+              .send(updateData)
+          })
+          .then(function(res) {
+            expect(res).to.have.status(204);
+          });
+      });
+      it('should delete recipes on DELETE', function() {
+        return chai.request(app)
+          .get('/recipes')
+          .then(function(res) {
+            return chai.request(app)
+              .delete(`/recipes/${res.body[0].id}`)
+          })
+          .then(function(res) {
+            expect(res).to.have.status(204);
+          });
+      });
+});
